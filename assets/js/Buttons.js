a38837eb6042988ad87e7c2491e3ff35409c49a6
kirk213 <karekun21@gmail.com>
2022-11-01 01:43:10 +0900

    update for mobile

--- assets/js/Buttons.js
+++ assets/js/Buttons.js
@@ -8,23 +8,25 @@ function submitBtn(e){
     //フォームの動作をキャンセル
     e.preventDefault();
     //入力されたタスクと日付の取得
-    let text = document.getElementById("newInput").value;
-    let date = document.getElementById("newDate").value;
+    let text = document.getElementById("newInput");
+    let date = document.getElementById("newDate");
 
     //テキストが入力されていない場合のアラート
-    if(text === ""){
+    if(text.value === ""){
         window.setTimeout(function(){alert("タスクを入力してください");}, 200);
         return;
     }//日付が入力されていない場合のアラート
-    if(date === ""){
+    if(date.value === ""){
         window.setTimeout(function(){alert("日付を入力してください。");}, 200);
         return;
     }
     //配列に追加
-    jsonArray.push({"text":text,"date":date,"check":false}); 
-    console.log(jsonArray);
+    jsonArray.push({"text":text.value,"date":date.value,"check":false}); 
     //ローカルストレージに保存
     localStorage.setItem('formData',JSON.stringify(jsonArray));
+    //入力欄をクリア
+    text.value="";
+    date.value="";
     //addTaskListを呼び出してタスクリストの再表示
     addTaskList();
 }
